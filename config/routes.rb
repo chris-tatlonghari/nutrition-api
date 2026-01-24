c4abc380cb4f3dc155a36c991cf12e5b276af4ee
chris.tatlonghari <christat98@gmail.com>
2026-01-24 16:57:23 -0500

    Separate home and service page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "services#index"
+  root "home#show"
 
   resources :services, only: %i[index show]
 end
