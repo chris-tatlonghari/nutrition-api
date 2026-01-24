cbe237ba3dfe14dbfe5f5548e2260b2e259bc92f
chris.tatlonghari <christat98@gmail.com>
2026-01-24 17:40:16 -0500

    Add model annotations

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,5 +1,36 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: food_items
+#
+#  id                :integer          not null, primary key
+#  calories          :integer
+#  calories_from_fat :integer
+#  carbs_g           :decimal(6, 2)
+#  cholesterol_mg    :integer
+#  fat_g             :decimal(6, 2)
+#  fiber_g           :decimal(6, 2)
+#  name              :string           not null
+#  protein_g         :decimal(6, 2)
+#  saturated_fat_g   :decimal(6, 2)
+#  serving_size      :decimal(8, 2)
+#  serving_unit      :string
+#  sodium_mg         :integer
+#  sugar_g           :decimal(6, 2)
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  service_id        :integer          not null
+#
+# Indexes
+#
+#  index_food_items_on_service_id           (service_id)
+#  index_food_items_on_service_id_and_name  (service_id,name) UNIQUE
+#
+# Foreign Keys
+#
+#  service_id  (service_id => services.id)
+#
 class FoodItem < ApplicationRecord
   
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: services
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Service < ApplicationRecord
   has_many :food_items
 
