706d7f4e16bd9d6c3669039105b4f44336376f65
chris.tatlonghari <christat98@gmail.com>
2026-02-02 08:58:37 -0500

    Add reset button

--- app/javascript/controllers/food_item_controller.js
+++ app/javascript/controllers/food_item_controller.js
@@ -11,4 +11,22 @@ export default class extends Controller {
             label.classList.remove('selected')
         }
     }
+
+    reset(event) {
+        event.preventDefault()
+
+        document.querySelectorAll('[data-food-item-card]').forEach(label => {
+            label.classList.remove('selected')
+            const checkbox = label.querySelector('input[type="checkbox"]')
+            if (checkbox) {
+                checkbox.checked = false
+            }
+        })
+
+        // Submit the form to update nutrition facts
+        const form = event.target.closest('form')
+        if (form) {
+            form.requestSubmit()
+        }
+    }
 }
